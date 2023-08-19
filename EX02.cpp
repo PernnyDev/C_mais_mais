392e7bb5f8fbc86a298f7e7ae6bbe4c30be2e084
Vinícius Nascimento Ribeiro <vinicius.ribeiro44@fatec.sp.gov.br>
2023-08-18 21:50:49 -0300

    Foi

---
+++ EX02.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main() {
+	int nr1;
+	int nr2;
+	
+	printf("Digite o nr1: ");
+	scanf("%i", &nr1);
+	printf("Digite nr2: ");
+	scanf("%i", &nr2);
+	printf("Voce digitou os numeros %i e %i", nr1, nr2);
+	printf(" Endereco na memoria do nr1 é %x:", &nr1);
+	printf(" Endereco na memoria do nr1 é %x:", &nr1);
+
+}
