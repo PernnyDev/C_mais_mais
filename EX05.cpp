2b6c4d33805cbdb1a26e1ed09a1f9ee40ddcc1ed
Vinícius Nascimento Ribeiro <137420040+PernnyDev@users.noreply.github.com>
2023-09-04 19:07:37 -0300

    Ultima aula 'switch'

--- EX05.cpp
+++ EX05.cpp
@@ -1,6 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 void inserirLista (int elem);
+void exibirLista (void);
+void qtdLista (void);
+
 
 struct No {
 	int info;
@@ -9,13 +12,13 @@ struct No {
 
 struct No *cabeca= NULL;
 struct No *ult = NULL;
-
-
+int qtd = 0;
+int nMenu;
 
 void inserirLista (int elem){
 	struct No *novo = (struct No*) malloc(sizeof(struct No ) ) ;
 	novo -> info = elem;
-	novo ->prox=NULL;
+	novo -> prox=NULL;
 	if (cabeca ==NULL){
 		cabeca = novo;
 	} else {
@@ -23,14 +26,54 @@ void inserirLista (int elem){
 		ult->prox = novo;
 	}
 	ult = novo;		
-	printf("%i\n ",  elem);
+
+}
+
+void exibirLista (void) {
+	
+	struct No *aux = cabeca;
+	while (aux!= NULL) {
+		printf("End.Atual: %x - Valor: %i -> Proxi.End: %x   \n ",  aux,  aux->info,aux -> prox);	
+		aux = aux -> prox;
+	}
 }
 
 
 
-main(){
-  inserirLista(10);
-  inserirLista(20);
-  inserirLista(30);
- 
+void qtdLista (void) {
+	struct No *aux = cabeca;
+	while (aux != NULL) {
+		qtd	+=1;
+		aux = aux -> prox;
+	}
+	printf("Quantidade total: %x   \n ",  qtd);	
 }
+
+
+		
+	
+
+
+main () {
+    printf("Digite: 1 - inserir 2 - exibir 3 - quantidade \n ");
+	scanf("%i", &nMenu);
+   
+switch ( nMenu) {
+	case 1 : inserirLista(10);
+	         inserirLista(20);
+  	break;
+	case 2 : 
+	           exibirLista();
+    case 3 : 
+               exibirLista();
+	break;
+}
+  
+  
+  
+	
+  
+  
+  
+}
+
